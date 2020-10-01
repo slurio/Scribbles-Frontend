895140b80da3406c5ce2818a811543bc3c7b9d7b
anickel101 <anson.nickel@gmail.com>
2020-09-30 22:34:05 -0400

    edit sound works with octave and note

--- circleCanvas.js
+++ circleCanvas.js
@@ -1,17 +1,22 @@
 class Circle {
-    constructor(posX, posY, dX, dY, radius, color, sound, context, id) {
+    constructor(posX, posY, dX, dY, radius, color, octave, note, context, id) {
         this.posX = posX,
         this.posY = posY,
         this.dX = dX,
         this.dY = dY,
         this.radius = radius,
         this.color = color,
-        this.sound = sound,
+        this.octave = octave,
+        this.note = note,
         this.context = context,
         this.id = id
     }
 
     draw() {
+        this.context.shadowBlur = 5;
+        this.context.shadowColor = "black";
+        // this.context.shadowOffsetX = 2;
+        // this.context.shadowOffsetY = 2;
         this.context.beginPath()
         this.context.fillStyle = this.color
         this.context.arc(this.posX, this.posY, this.radius, 0, Math.PI * 2)
@@ -30,10 +35,11 @@ class Circle {
     checkBoundaries(canvas) {
         if(this.posY + this.dY > canvas.height || this.posY + this.dY < 0) {
             this.dY = -this.dY;
-            tones.play(this.sound);
+            tones.play(this.note, this.octave);
+            console.log("tone played for Y boundary", this.note, this.octave)
         } else if (this.posX + this.dX > canvas.width || this.posX + this.dX < 0) {
             this.dX = -this.dX; 
-            tones.play(this.sound);
+            tones.play(this.note, this.octave);
         }
     }
 }
